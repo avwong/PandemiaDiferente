fe13136f0f90c74dd020193b847aae54ada922b8
avwong <125937944+avwong@users.noreply.github.com>
2025-10-17 17:43:35 -0600

    Agregar archivo random.h

---
+++ random.h
@@ -0,0 +1,11 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <stdlib.h>
+#include <time.h>
+
+void crear_random(unsigned int seed); //inicializa el generador de números aleatorios con una semilla
+int random_int(int min, int max); //devuelve un número entero aleatorio entre min y max
+void mezclar_arreglo(int* arreglo, int totalNumeros); //mezcla un arreglo de enteros para luego seleccionar países al azar al mismo tiempo (sin que se repitan)
+
+#endif
