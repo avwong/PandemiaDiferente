ec3d965d3d8e683bdc46b81c97e1550163f4708b
Alina <125937944+avwong@users.noreply.github.com>
2025-10-18 01:10:21 -0600

    Nombre mas completo de funcion intervalo

--- random.h
+++ random.h
@@ -5,8 +5,9 @@
 #include <time.h>
 
 void crear_random(); //inicializa el generador de números aleatorios
-int random_int(int min, int max); //devuelve un número entero aleatorio entre min y max
+int random_intervalo(int min, int max); //devuelve un número entero aleatorio entre min y max
 void mezclar_arreglo(int* arreglo, int totalNumeros); //mezcla un arreglo de enteros para luego seleccionar países al azar al mismo tiempo (sin que se repitan)
 
 
 #endif
+
